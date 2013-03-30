7a8a4c1547ef816414dd041438c8b2f8beb49715
Matthew Davey <matthewd@project-2501.net>
2013-03-30 17:15:25 +1100

    Added command line parser

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 __author__ = 'Matthew'
 
 import os
+import argparse
 
 from page import Page
 
@@ -15,5 +16,14 @@ def download(host, page, directory):
     for image in page.get_images():
         image.download_to(directory)
 
-# download('boards.4chan.org', '/a/res/82138812', 'z:\\foo')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Download all images from a 4chan thread to a specific directory')
+    parser.add_argument('thread', help='Source thread.  e.g. /a/res/82138812')
+    parser.add_argument('dest', help='Destination directory.  e.g. c:/images')
+    # parser.add_argument('--board', default='boards.4chan.org')
+    args = parser.parse_args()
+
+    # download('boards.4chan.org', '/a/res/82138812', 'z:\\foo')
+    download('boards.4chan.org', args.thread, args.dest)
 
